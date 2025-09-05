863e84fd1cd912df3436daa5e333fda505c9ff34
Abdullah Mahmoud <almatador.egy@gmail.com>
2025-09-05 22:54:43 +0300

    Adding the get user notifications function

--- Client/src/services/user.ts
+++ Client/src/services/user.ts
@@ -21,3 +21,9 @@ export async function getUserPosts(userId: number) {
     if (!res.ok) throw new Error("Failed to fetch posts");
     return res.json();
 }
+
+export async function getUserNotifications(userId: number) {
+    const res = await fetch(`http://127.0.0.1:8000/get-notifications?user_id=${userId}`);
+    if (!res.ok) throw new Error("Failed to fetch notifications");
+    return res.json();
+}
