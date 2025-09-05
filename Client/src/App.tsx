0e5b766d78bf10b1ec63aa73d842962e44161ec5
Abdullah Mahmoud <almatador.egy@gmail.com>
2025-09-05 14:07:36 +0300

    Adding the login page

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing/Landing";
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
+      <Route path="/login" element={<Login />} />
       {/* Later you can add: <Route path="/chat" element={<Chat />} /> */}
     </Routes>
   );
---
+++ Client/src/pages/Login/Login.tsx
@@ -0,0 +1,21 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+export default function Login() {
+    return (
+        <div className="w-screen h-screen flex items-center justify-center">
+            <div className="flex flex-col items-center justify-center h-auto gap-4 w-120 px-16">
+                <div className="w-full flex items-center justify-start mb-6"><button className="h-12 w-12 flex items-center justify-center bg-green-400 rounded-md hover:bg-green-500 cursor-pointer"><FontAwesomeIcon icon={faArrowLeft} className="text-white"/></button></div>
+                <h1 className="text-5xl">VibeConnect</h1>
+                <p className="text-gray-500 mb-6">Sign in to your account</p>
+                <input type="text" placeholder="Enter your email" className="w-full rounded-md border border-gray-300 h-12 px-2 hover:border-green-400 focus:border-green-500"/>
+                <input type="text" placeholder="Enter your password" className="w-full rounded-md border border-gray-300 h-12 px-2 hover:border-green-400 focus:border-green-500"/>
+                <div className="w-full  mb-6"><a href="" className="text-green-500">Forgot your password?</a></div>
+                <button className="w-full bg-green-400 h-12 text-white rounded-md hover:bg-green-500 cursor-pointer">Sign In</button>
+                <p className="text-gray-500">Or with</p>
+                <button className="w-full rounded-md border border-gray-300 h-12 hover:border-gray-500 hover:bg-gray-50 cursor-pointer">Sign in with google</button>
+                <button className="w-full rounded-md border border-gray-300 h-12 hover:border-gray-500 hover:bg-gray-50 cursor-pointer">Sign in with tweeter</button>
+                <p className="text-gray-500 mt-6">Don't have an account?, let's <a href="" className="text-green-500">Sign Up</a></p>
+            </div>
+        </div>
+    )
+}
